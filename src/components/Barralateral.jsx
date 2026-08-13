6a604eb68f8adde899d4822810da287a9cc1ea10
law <projetofisica98@gmail.com>
2026-08-13 15:23:12 -0300

    Sobre Nós atualizado, falta fts

--- src/components/Barralateral.jsx
+++ src/components/Barralateral.jsx
@@ -29,8 +29,10 @@ function BarraLateral(){
             {/*A navegação tera emojis ao lado dos nomes, referente a página, ex.: Inicio > Casa 
             Todos alinhados iguais e com hover, com destaque em qual página está no momento.*/}
                 <div className="Top">
-                    {/*<img src="https://imgs.search.brave.com/W4G6kU1IioffVGmnPRo05eCxUjaI3-rLtQHdu5qpUhQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9uZXR3/b3JrLmdydXBvYWJy/aWwuY29tLmJyL3dw/LWNvbnRlbnQvdXBs/b2Fkcy9zaXRlcy80/LzIwMTYvMDgvMTMx/LnBuZz9xdWFsaXR5/PTcw" alt="" />*/}
+                    {/*
                     <img src={Image} alt="" />
+                    */}
+                    <img src="https://imgs.search.brave.com/W4G6kU1IioffVGmnPRo05eCxUjaI3-rLtQHdu5qpUhQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9uZXR3/b3JrLmdydXBvYWJy/aWwuY29tLmJyL3dw/LWNvbnRlbnQvdXBs/b2Fkcy9zaXRlcy80/LzIwMTYvMDgvMTMx/LnBuZz9xdWFsaXR5/PTcw" alt="" />
                     <h3>Confira as outras páginas</h3>
                 </div>
 
@@ -39,9 +41,9 @@ function BarraLateral(){
                         <ul className="navegacao">
                             <li><Link to="/">Inicio</Link></li>
                             <li><Link to="/Simulador">Simulador</Link></li>
-                            <li><Link to="/Altura">Altura</Link></li>
+                           {/*  <li><Link to="/Altura">Altura</Link></li>
                             <li><Link to="/Alcance">Alcance</Link></li>
-                            <li><Link to="/Tempo">Tempo</Link></li>
+                            <li><Link to="/Tempo">Tempo</Link></li> */}
                             <li><Link to="/Exemplos">Como usar</Link></li>
                             <li><Link to="/Sobre">Sobre</Link></li>
                         </ul>
